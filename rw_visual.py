3699c8b4079ae75c4c31ac2bc180c1a46b436511
Thomas Ngo <tngo0508@gmail.com>
2018-08-16 11:51:43 -0700

    cleaning up the axes

--- rw_visual.py
+++ rw_visual.py
@@ -16,7 +16,12 @@ while True:
     # Emphasize the first and last points.
     plt.scatter(0, 0, c='green', edgecolors=None, s=100)
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', cmap=plt.cm.Blues,
-                edgecolor='none', s=15)
+                edgecolor='none', s=100)
+
+    # Remove the axes.
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+
     plt.show()
 
     KEEP_RUNNING = raw_input("Make another walk?(y/n): ")
