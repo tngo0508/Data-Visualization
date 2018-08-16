f8c4932a17966af4fa2ae0a2588f7333d0d1a5cc
Thomas Ngo <tngo0508@gmail.com>
2018-08-16 09:48:21 -0700

    add random_walk class, fix save image

---
+++ random_walk.py
@@ -0,0 +1,11 @@
+from random import choice
+
+class RandomWalk(object):
+    """A class to generate random walks."""
+
+    def __init__(self, num_points=5000):
+        self.num_points = num_points
+
+        # All walks start at (0,0)
+        self.x_values = [0]
+        self.y_valus = [0]
