c86710b6bf0934b8dbdd971898c63f04a051045c
Thomas Ngo <tngo0508@gmail.com>
2018-08-16 18:09:17 -0700

    Adding error-checking

--- highs_lows.py
+++ highs_lows.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from matplotlib import pyplot as plt
 
 # Get dates, high, and low temperatures from file
-filename = 'sitka_weather_2014.csv'
+filename = 'death_valley_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -14,14 +14,16 @@ with open(filename) as f:
 
     dates, highs, lows = [], [], []
     for row in reader:
-        current_date = datetime.strptime(row[0], "%Y-%m-%d")
-        dates.append(current_date)
-
-        high = int(row[1])
-        highs.append(high)
-
-        low = int(row[3])
-        lows.append(low)
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print current_date, 'missing data'
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 # Plot data.
 fig = plt.figure(dpi=128, figsize=(10, 6))
@@ -30,7 +32,8 @@ plt.plot(dates, lows, c='blue', alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot.
-plt.title("Daily high and low temperatures - 2014", fontsize=24)
+plt.title("Daily high and low temperatures - 2014\nDeath Valley, CA",
+          fontsize=20)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
