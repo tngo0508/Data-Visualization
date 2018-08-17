bde08ae5cc1ecc0f5e1200a8a4faf9a2fed62403
Thomas Ngo <tngo0508@gmail.com>
2018-08-16 17:46:28 -0700

    Plotting a second data series

--- highs_lows.py
+++ highs_lows.py
@@ -3,8 +3,8 @@ from datetime import datetime
 
 from matplotlib import pyplot as plt
 
-# Get dates and high temperatures from file
-filename = 'sitka_weather_07-2014.csv'
+# Get dates, high, and low temperatures from file
+filename = 'sitka_weather_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -12,7 +12,7 @@ with open(filename) as f:
     # for index, column_header in enumerate(header_row):
     #     print index, column_header
 
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[0], "%Y-%m-%d")
         dates.append(current_date)
@@ -20,14 +20,18 @@ with open(filename) as f:
         high = int(row[1])
         highs.append(high)
 
+        low = int(row[3])
+        lows.append(low)
+
     print highs
 
 # Plot data.
 fig = plt.figure(dpi=128, figsize=(10, 6))
 plt.plot(dates, highs, c='red')
+plt.plot(dates, lows, c='blue')
 
 # Format plot.
-plt.title("Daily high temperatures, July 2014", fontsize=24)
+plt.title("Daily high and low temperatures - 2014", fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
