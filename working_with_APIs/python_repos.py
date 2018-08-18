369d157bf9e82a7203bebe10d49590a0b9c6f6ac
Thomas Ngo <tngo0508@gmail.com>
2018-08-18 08:48:24 -0700

    Working with the response dictionary

--- working_with_APIs/python_repos.py
+++ working_with_APIs/python_repos.py
@@ -7,6 +7,14 @@ print("Status code:", r.status_code)
 
 # Store API response in a variable.
 response_dict = r.json()
+print("Total repositories:", response_dict['total_count'])
 
-# Process results.
-print(response_dict.keys())
+# Explore information about the repositories.
+repo_dicts = response_dict['items']
+print("repositories returned:", len(repo_dicts))
+
+# Examine the first repository.
+repo_dict = repo_dicts[0]
+print "\nKeys:", len(repo_dict)
+for key in sorted(repo_dict.keys()):
+    print(key)
