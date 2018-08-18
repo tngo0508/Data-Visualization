12dc141df18fa5f84e2d351f91e9e18d351feae7
Thomas Ngo <tngo0508@gmail.com>
2018-08-17 23:43:15 -0700

    lightening the color theme

--- world_population.py
+++ world_population.py
@@ -1,7 +1,7 @@
 import json
 
 import pygal
-from pygal.style import RotateStyle
+from pygal.style import RotateStyle as RS, LightColorizedStyle as LCS
 from country_codes import get_country_code
 
 # Load the data into a list.
@@ -34,7 +34,7 @@ for cc, pop in cc_populations.items():
 # See how many countries are in each level.
 print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
 
-wm_style = RotateStyle('#336699')
+wm_style = RS('#336699', base_style=LCS)
 wm = pygal.maps.world.World(style=wm_style)
 wm.title = 'World Population in 2010, by Country'
 wm.add('0-10m', cc_pops_1)
